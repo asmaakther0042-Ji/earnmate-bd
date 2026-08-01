4c05950bc148364c0af6d90175b7d497e6de6c79
asmaakther0042-Ji <asmaakther0042@gmail.com>
2026-08-01 19:17:15 +0600

    Add app.dart with MaterialApp configuration

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 
+class EarnMateApp extends StatelessWidget {
+  const EarnMateApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EarnMate BD',
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+      ),
+
+      home: const SplashScreen(),
+    );
+  }
+}
