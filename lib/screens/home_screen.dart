0250e239923e6e186e4376db20bd96b5d47af871
asmaakther0042-Ji <asmaakther0042@gmail.com>
2026-08-03 17:06:42 +0600

    Add home screen UI
    Created the home screen with app bar and welcome message.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("EarnMate BD"),
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome to EarnMate BD!",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
