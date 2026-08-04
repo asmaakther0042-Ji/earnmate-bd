92abb322da57c2f5625f1cd42f296124aebf9caa
asmaakther0042-Ji <asmaakther0042@gmail.com>
2026-08-04 14:18:06 +0600

    Connect Home Screen to Quiz Screen
    Added Start Quiz button and connected HomeScreen with QuizScreen.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quiz_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,13 +9,51 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("EarnMate BD"),
+        centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          "Welcome to EarnMate BD!",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.school,
+                size: 90,
+                color: Colors.green,
+              ),
+
+              const SizedBox(height: 20),
+
+              const Text(
+                "Welcome to EarnMate BD",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              SizedBox(
+                width: double.infinity,
+                height: 55,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const QuizScreen(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Start Quiz",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
